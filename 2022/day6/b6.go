a54c54bb727d4d694b4df365cb8da2ea6f80413d
= <tristanmayofreire@gmail.com>
2023-11-23 13:36:09 +0100

    day 6

---
+++ 2022/day6/b6.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./day6/input.txt")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	defer file.Close()
+	// what ends up on top
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		startPaket := -1
+		line := scanner.Text()
+		// line := "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+
+		nCharsInPacketPrefix := 14
+		idx := 0
+		for idx < len(line)-nCharsInPacketPrefix {
+			chars := []rune{}
+			for i := 0; i < nCharsInPacketPrefix; i++ {
+				charIdx := idx + i
+				// fmt.Println(chars)
+				chars = append(chars, rune(line[charIdx]))
+			}
+			// fmt.Println(chars)
+			if areAllUnique(chars) {
+				// fmt.Println("INDEX IS: ", idx)
+				startPaket = idx + nCharsInPacketPrefix
+				break
+			}
+			idx += 1
+
+		}
+		fmt.Println(startPaket)
+	}
+
+}
+
+func areAllUnique(charList []rune) bool {
+	uniqueChars := make(map[rune]bool)
+	for _, char := range charList {
+		if uniqueChars[char] {
+			return false
+		}
+		uniqueChars[char] = true
+	}
+	return true
+}
+
+// 4 unique characters in a row represents end of subroutine. Return n of characters processed before start of sequence
